5dcaa79779273608d913a5daae1004481f9bac48
Brian <lam.b@husky.neu.edu>
2019-02-25 17:37:28 -0800

    finished changing the routes

--- src/app/views/website/website-list/website-list.component.ts
+++ src/app/views/website/website-list/website-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Website} from '../../../models/website.model.client';
+import {WebsiteService} from '../../../services/website.service.client';
+
 
 @Component({
   selector: 'app-website-list',
   templateUrl: './website-list.component.html',
   styleUrls: ['./website-list.component.css']
 })
-export class WebsiteListComponent implements OnInit {
 
-  constructor() { }
+export class WebsiteListComponent implements OnInit {
+  uid: string;
+  websites: Website[] = [];
+
+  constructor(private websiteService: WebsiteService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(
+      (params: any) => {
+        this.uid = params['uid'];
+      });
+    this.websites = this.websiteService.findWebsitesByUser(this.uid);
   }
-
 }
--- src/app/views/website/website-new/website-new.component.ts
+++ src/app/views/website/website-new/website-new.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {Website} from '../../../models/website.model.client';
+import {NgForm} from '@angular/forms';
+import {WebsiteService} from '../../../services/website.service.client';
 
 @Component({
   selector: 'app-website-new',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WebsiteNewComponent implements OnInit {
 
-  constructor() { }
+  uid: string;
+  newWebsite: Website;
+  newWebsiteName: string;
+  newWebsiteDescription: string;
+  websites: Website[];
+
+  constructor(private websiteService: WebsiteService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.uid = params['uid'];
+        }
+      );
+    this.websites = this.websiteService.findWebsitesByUser(this.uid);
   }
 
+  onSubmit() {
+    this.newWebsite = {_id: '', name: this.newWebsiteName, developerId: this.uid, description: this.newWebsiteDescription};
+    this.websiteService.createWebsite(this.uid, this.newWebsite);
+    this.router.navigate(['../'], {relativeTo: this.route});
+  }
 }
